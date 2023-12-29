30398bbad2f9312061fb1070e0d032730665790f
jayrboy <j.jayox_@hotmail.com>
2023-12-29 22:38:57 +0700

    Add Component Header.js AddForm.js Item.js
    Add Component Header.js AddForm.js Item.js

---
+++ src/components/AddForm.js
@@ -0,0 +1,13 @@
+export default function AddForm() {
+  return (
+    <>
+      <h2>แบบฟอร์มจัดการงาน</h2>
+      <form className="form-group">
+        <input type="text" className="text-input" />
+        <button type="submit" className="submit-btn">
+          เพิ่ม
+        </button>
+      </form>
+    </>
+  );
+}
