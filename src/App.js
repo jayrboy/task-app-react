0c4cf43dc8fe595722649e6d08afbd627699e4f2
jayrboy <j.jayox_@hotmail.com>
2023-12-29 23:18:25 +0700

    Create State
    Create State

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import "./App.css";
 import Header from "./components/Header";
 import AddForm from "./components/AddForm";
 import Item from "./components/Item";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState([
+    { id: 1, title: "แก้บั๊คโปรแกรม" },
+    { id: 2, title: "คู่มือการใช้งาน" },
+    { id: 3, title: "กดเงินธนาคาร" },
+  ]);
+
   return (
     <div className="App">
       <Header />
