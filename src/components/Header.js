c15b74442607e0199d00372e79f7c26cbd17ee66
jayrboy <j.jayox_@hotmail.com>
2023-12-30 14:39:19 +0700

    Add ToggleTheme
    Add ToggleTheme

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,14 @@ import "./Header.css";
 export default function Header(props) {
   const { theme, setTheme } = props;
 
+  function toggleTheme() {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }
+
   return (
     <header>
       <div className="logo">
@@ -10,7 +18,9 @@ export default function Header(props) {
       </div>
       <div className="theme-container">
         <span>{theme === "light" ? "โหมดกลางวัน" : "โหมดกลางคืน"}</span>
-        <span className="icon">สลับ</span>
+        <span className="icon" onClick={toggleTheme}>
+          สลับ
+        </span>
       </div>
     </header>
   );
