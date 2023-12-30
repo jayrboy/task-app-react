d1e819be63816cdccf774689e874c1530bf71b38
jayrboy <j.jayox_@hotmail.com>
2023-12-30 14:45:26 +0700

    Theme Style
    Theme Style

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={"App " + theme}>
       <Header theme={theme} setTheme={setTheme} />
       <div className="container">
         <AddForm
